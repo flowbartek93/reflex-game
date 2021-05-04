70e2feec755f4d81e19711150c3e98ded6dafb6e
Bartek <submission93@gmail.com>
2021-05-04 18:25:19 +0200

    fixed bug with stopwatch

--- final.js
+++ final.js
@@ -1,65 +1,55 @@
-const restartBtn = document.createElement("button");
-const messageBoard = document.querySelector('.section-btns');
+function createRestartBtn() {
+  const messageBoard = document.querySelector(".section-btns");
+  const restartBtn = document.createElement("button");
+  messageBoard.appendChild(restartBtn);
+  restartBtn.textContent = "restart";
+  restartBtn.classList.add("restart-btn");
 
-
+  restartBtn.addEventListener("click", restartGame);
+}
 
 function checkIfComplete() {
+  console.log("checking");
+  // Check if main cat board DIV conatins no childrens(cats)
+  if (catBoard.children.length === 0) {
+    win = true;
+    const stopwatchContainer = document.querySelector(".watch-container");
+    document.querySelector(".section-btns p").textContent = `You are pretty fast ! Your time is: ${stopwatchContainer.textContent}`;
+    stopwatchContainer.remove();
 
+    createRestartBtn();
+  }
 
+  if (win === false) {
+    const stopwatchContainer = document.querySelector(".watch-container");
+    const catsLeft = catBoard.querySelectorAll(".cat");
+    catsLeft.forEach(cat => {
+      cat.style.position = null;
+      cat.style.top = null;
+      cat.style.left = null;
+      cat.classList.remove("draggable");
+    });
+    document.querySelector(".section-btns p").textContent = `You slow loser ! Your time is: ${stopwatchContainer.textContent}`;
+    stopwatchContainer.remove();
 
-    // Check if main cat board DIV conatins no childrens(cats)
-    if (catBoard.children.length === 0) {
-        win = true;
-    }
-
-    if (win === false) {
-
-        const stopwatch = document.querySelector('.watch-container');
-        messageBoard.appendChild(restartBtn);
-        restartBtn.textContent = "restart"
-        restartBtn.classList.add("restart-btn")
-        document.querySelector('.section-btns p').textContent = `You slow loser ! Your time is: ${stopwatch.textContent}`;
-        stopwatch.remove();
-
-        restartBtn.addEventListener("click", restartGame)
-
-    } else if (win === true) {
-
-        const stopwatch = document.querySelector('.watch-container');
-        messageBoard.appendChild(restartBtn);
-        restartBtn.textContent = "restart"
-        restartBtn.classList.add("restart-btn")
-        document.querySelector('.section-btns p').textContent = `You are pretty fast ! Your time is: ${stopwatch.textContent}`;
-        stopwatch.remove();
-
-        restartBtn.addEventListener("click", restartGame)
-
-    }
-
+    createRestartBtn();
+  }
 }
 
 function restartGame() {
+  const catsToBeRemoved = document.querySelector(".shadow-cats-container");
+  document.querySelector(".cat-shadow-board").removeChild(catsToBeRemoved);
+  buttonsContainer.style.display = "flex";
+  document.querySelector(".restart-btn").remove();
+  document.querySelector(".section-btns p").remove();
 
+  while (catBoard.hasChildNodes()) {
+    catBoard.removeChild(catBoard.childNodes[0]);
+  }
 
-    const catsToBeRemoved = document.querySelector('.shadow-cats-container')
-    document.querySelector('.cat-shadow-board').removeChild(catsToBeRemoved);
-    buttonsContainer.style.display = "flex";
-    restartBtn.remove();
-    document.querySelector('.section-btns p').remove();
-
-
-
-    while (catBoard.hasChildNodes()) {
-        catBoard.removeChild(catBoard.childNodes[0]);
-    }
-
-    win = null;
-    idCounter = 0;
-    document.removeEventListener("mousemove", onMouseMove)
-    document.removeEventListener("mousedown", onMouseDown)
-    document.removeEventListener("mousemove", onMouseUp)
-
-
-
-
+  win = null;
+  idCounter = 0;
+  document.removeEventListener("mousemove", onMouseMove);
+  document.removeEventListener("mousedown", onMouseDown);
+  document.removeEventListener("mousemove", onMouseUp);
 }
--- main.js
+++ main.js
@@ -1,230 +1,177 @@
 // https://www.mozilla.org/en-US/MPL/2.0/
 
-const section = document.querySelector('.section-btns'); //przyciski
-const addBtn = document.querySelector('.add-btn') // button dodający kota
-const startBtn = document.querySelector('.start-btn') //button startujący grę
-const catBoard = document.querySelector('.cat-board'); //flexbox z kotami
-const buttonsContainer = document.querySelector('.btn-container');
+const section = document.querySelector(".section-btns"); //przyciski
+const addBtn = document.querySelector(".add-btn"); // button dodający kota
+const startBtn = document.querySelector(".start-btn"); //button startujący grę
+const catBoard = document.querySelector(".cat-board"); //flexbox z kotami
+const buttonsContainer = document.querySelector(".btn-container");
 
 let idCounter = 0;
 let win = null;
 
-function downloadCat() { // Funkcja będzie pobierać koty z serwera (fetch api) np. 100 kotów, ale wylosuje tylko 6 i na ich bazie stworzy elementy html.
+function downloadCat() {
+  // Funkcja będzie pobierać koty z serwera (fetch api) np. 100 kotów, ale wylosuje tylko 6 i na ich bazie stworzy elementy html.
 
-    const catsUrl = "https://api.thecatapi.com/v1/images/search";
-    fetch(catsUrl).then(response => {
-            return response.json();
-        })
-        .then(json => displayCat(json[0].url))
-
+  const catsUrl = "https://api.thecatapi.com/v1/images/search";
+  fetch(catsUrl)
+    .then(response => {
+      return response.json();
+    })
+    .then(json => displayCat(json[0].url));
 }
-
 
 function displayCat(imgUrl) {
-    // podanie w funkcji pobranego url, który potem jest umieszczany w css
+  // podanie w funkcji pobranego url, który potem jest umieszczany w css
 
-    if (catBoard.children.length >= 6) {
-        alert('too many cats')
-        return;
-    }
+  if (catBoard.children.length >= 6) {
+    alert("You can only have 6 cats on the board");
+    return;
+  }
 
-    const newCat = document.createElement("div");
-    newCat.setAttribute("class", "cat draggable")
-    /* Loading */
-    const loading = document.createElement('div')
-    loading.setAttribute("class", "lds-ring")
-    newCat.appendChild(loading);
-    for (let i = 0; i < 4; i++) {
-        const emptyDiv = document.createElement('div');
-        loading.appendChild(emptyDiv);
-    }
+  const newCat = document.createElement("div");
+  newCat.setAttribute("class", "cat draggable");
+  /* Loading */
+  const loading = document.createElement("div");
+  loading.setAttribute("class", "lds-ring");
+  newCat.appendChild(loading);
+  for (let i = 0; i < 4; i++) {
+    const emptyDiv = document.createElement("div");
+    loading.appendChild(emptyDiv);
+  }
 
-    let image = new Image();
-    image.src = imgUrl
-    image.onload = function () {
-        loading.remove();
-    }
-    /* Loading */
+  let image = new Image();
+  image.src = imgUrl;
+  image.onload = function () {
+    loading.remove();
+  };
+  /* Loading */
 
+  newCat.setAttribute("id", idCounter++);
+  newCat.style.backgroundImage = "url(" + image.src + ")";
 
-    newCat.setAttribute("id", idCounter++);
-    newCat.style.backgroundImage = "url(" + image.src + ")";
-
-
-
-    catBoard.appendChild(newCat);
-
+  catBoard.appendChild(newCat);
 }
-
 
 /* Wyświetlenie stopera */
 
 function drawStopwatch() {
-
-    const watchContainer = document.createElement('div');
-    watchContainer.classList.add('watch-container');
-    watchContainer.innerHTML = `<span class="seconds">00</span>:<span class="miliseconds">00</span>`;
-    section.insertAdjacentElement('beforeend', watchContainer);
-
-
+  const watchContainer = document.createElement("div");
+  watchContainer.classList.add("watch-container");
+  watchContainer.innerHTML = `<span class="seconds">00</span>:<span class="miliseconds">00</span>`;
+  section.insertAdjacentElement("beforeend", watchContainer);
 }
 
 /* Stoper */
 
 function stopwatch() {
+  const secondsHandler = document.querySelector(".seconds");
+  const milisecondsHandler = document.querySelector(".miliseconds");
+  const maxtime = setDifficulty();
+  let seconds = 0;
+  let milsecs = 0;
 
-    const secondsHandler = document.querySelector('.seconds')
-    const milisecondsHandler = document.querySelector('.miliseconds')
+  /* Checking if allowed time has been exceed */
+  const timerSecs = setInterval(() => {
+    const totaltime = parseInt(secondsHandler.textContent + milisecondsHandler.textContent) / 100;
 
-    const maxtime = setDifficulty();
-    console.log(maxtime);
+    if (totaltime >= maxtime && win === false) {
+      clearInterval(timerSecs);
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+      document.removeEventListener("mousedown", onMouseDown);
+      /* Przegrana */
+      win = false;
+      console.log(win);
+      checkIfComplete();
+    }
 
-    let seconds = 0;
-    let milsecs = 0;
+    /* Simulation of stopwatch */
 
+    milsecs++;
 
+    if (milsecs < 9) {
+      milisecondsHandler.innerHTML = "0" + milsecs;
+    }
 
-    /* Checking if allowed time was exceed */
-    const timerSecs = setInterval(() => {
+    if (milsecs > 9) {
+      milisecondsHandler.innerHTML = milsecs;
+    }
 
-        (function () {
-            const totaltime = parseInt(secondsHandler.textContent + milisecondsHandler.textContent) / 100;
+    if (milsecs > 99) {
+      seconds++;
+      secondsHandler.innerHTML = "0" + seconds;
+      milsecs = 0;
+      milisecondsHandler.innerHTML = "0" + 0;
+    }
 
-
-            if (totaltime >= maxtime) {
-                const catsLeft = catBoard.querySelectorAll('.cat');
-                clearInterval(timerSecs);
-                document.removeEventListener("mousemove", onMouseMove)
-                document.removeEventListener("mouseup", onMouseUp)
-                document.removeEventListener("mousedown", onMouseDown)
-                /* Przegrana */
-                if (!win === true) {
-                    win = false
-                    checkIfComplete();
-                    catsLeft.forEach(cat => {
-                        cat.style.position = null;
-                        cat.style.top = null;
-                        cat.style.left = null;
-                        cat.classList.remove('draggable');
-                    })
-                    return;
-                } else {
-
-                    win = null;
-                }
-                console.log(win);
-
-            }
-        }());
-
-
-        /* Simulation of stopwatch */
-
-        milsecs++
-
-        if (milsecs < 9) {
-            milisecondsHandler.innerHTML = "0" + milsecs;
-        }
-
-        if (milsecs > 9) {
-            milisecondsHandler.innerHTML = milsecs;
-
-        }
-
-        if (milsecs > 99) {
-            seconds++;
-            secondsHandler.innerHTML = "0" + seconds;
-            milsecs = 0;
-            milisecondsHandler.innerHTML = "0" + 0;
-        }
-
-        if (seconds > 9) {
-            secondsHandler.innerHTML = seconds;
-        }
-
-    }, 10)
-
+    if (seconds > 9) {
+      secondsHandler.innerHTML = seconds;
+    }
+  }, 10);
 }
 
-
 function setDifficulty() {
+  let catsNumber = catBoard.children.length;
+  let difficulty = 1.2 * catsNumber;
 
-    let catsNumber = catBoard.children.length;
-    let difficulty = 1.2 * catsNumber;
-
-    return difficulty;
-
+  return difficulty;
 }
 
 function startGame() {
+  win = null;
+  let catsNumber = catBoard.children.length;
 
-    win = null;
-    let catsNumber = catBoard.children.length;
+  //sprawdzenie czy są dodane jakieś koty - jeśli nie to startGame() nie może się dalej wykonać
+  if (catsNumber === 0) {
+    alert("Please add some cats");
+    return;
+  } else if (catsNumber > 0) {
+    const paragraph = document.createElement("p");
+    buttonsContainer.style.display = "none";
+    paragraph.textContent = "Fit cats to their matchning clones !";
+    section.appendChild(paragraph);
+  }
 
-    //sprawdzenie czy są dodane jakieś koty - jeśli nie to startGame() nie może się dalej wykonać
-    if (catsNumber === 0) {
-        alert("Musisz dodać jakieś koty !!!");
-        return;
-    } else if (catsNumber > 0) {
+  createShadowCats(); // odpalenie drugiej planszy na cienie kotów
+  drawStopwatch(); // rysuj stoper
+  stopwatch(); // odpal stoper
 
-        const paragraph = document.createElement("p");
-        buttonsContainer.style.display = "none";
-        paragraph.textContent = "Fit cats to their matchning clones !"
-        section.appendChild(paragraph);
-
-    }
-
-    createShadowCats(); // odpalenie drugiej planszy na cienie kotów
-    drawStopwatch(); // rysuj stoper
-    stopwatch(); // odpal stoper
-
-    document.addEventListener("mousedown", onMouseDown); // 
+  document.addEventListener("mousedown", onMouseDown); //
 }
-
-
-
 
 /* Top shadow cats board */
 
 function createShadowCats() {
-    const shadowCatsBoard = document.querySelector('.cat-shadow-board'); // główny kontener html
-    const catBoardCopy = catBoard.cloneNode(true); // kopia catBoard ze jego dziećmi(koty)
-    const shadowCatsContainer = document.createElement("div");
-    shadowCatsBoard.appendChild(shadowCatsContainer);
-    shadowCatsContainer.classList.add("shadow-cats-container");
+  const shadowCatsBoard = document.querySelector(".cat-shadow-board"); // główny kontener html
+  const catBoardCopy = catBoard.cloneNode(true); // kopia catBoard ze jego dziećmi(koty)
+  const shadowCatsContainer = document.createElement("div");
+  shadowCatsBoard.appendChild(shadowCatsContainer);
+  shadowCatsContainer.classList.add("shadow-cats-container");
 
-    const catArrayList = [...catBoardCopy.querySelectorAll('.cat')];
-    catArrayList.forEach(item => {
+  const catArrayList = [...catBoardCopy.querySelectorAll(".cat")];
+  catArrayList.forEach(item => {
+    item.style.order = null;
+    item.removeAttribute("id");
+    item.style.opacity = "0.2";
+    item.classList.remove("draggable");
+  });
 
+  let numbers = [...Array(catArrayList.length).keys()];
 
-        item.style.order = null;
-        item.removeAttribute("id")
-        item.style.opacity = "0.2";
-        item.classList.remove("draggable");
+  function randomIndex() {
+    return Math.floor(Math.random() * catArrayList.length - 1 + 1);
+  }
 
-    })
-
-    let numbers = [...Array(catArrayList.length).keys()];
-
-    function randomIndex() {
-        return Math.floor((Math.random() * catArrayList.length - 1) + 1);
-    }
-
-    numbers.map(function () {
-
-        const index = randomIndex();
-        shadowCatsContainer.appendChild(catArrayList[index]);
-        catArrayList.splice(index, 1);
-
-    })
-
+  numbers.map(function () {
+    const index = randomIndex();
+    shadowCatsContainer.appendChild(catArrayList[index]);
+    catArrayList.splice(index, 1);
+  });
 }
-
 
 /*Buttons add cat and start game*/
 
 document.addEventListener("DOMContentLoaded", function () {
-    addBtn.addEventListener("click", downloadCat)
-    startBtn.addEventListener("click", startGame)
-
-})
+  addBtn.addEventListener("click", downloadCat);
+  startBtn.addEventListener("click", startGame);
+});
